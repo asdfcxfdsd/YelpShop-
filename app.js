cd7eeece18975f3b28d4f7e699a9806a1fe7315b
Eden <eden@EdendeMacBook-Air.local>
2022-08-21 14:54:18 +0800

    make it better

--- app.js
+++ app.js
@@ -227,18 +227,9 @@ app.use((err, req, res, next) => {
 
 
 
-
-
-
-
-
-
-
-
-
 const port =  process.env.PORT ||3000; 
 
 
 app.listen(port, () => {
-    console.log('It worked!!!')
+    console.log(`${port} IS WORKING~`)
 })
